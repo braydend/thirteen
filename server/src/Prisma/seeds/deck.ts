17d4a169e9aedaede74dca87e61020a8eca37992
Brayden <braydendiluca@gmail.com>
2020-08-09 21:40:43 +1000

    Create card seeder

--- server/src/Prisma/seeds/deck.ts
+++ server/src/Prisma/seeds/deck.ts
@@ -26,6 +26,23 @@ const cardValues = [
 const seedDeck = async (client: PrismaClient): Promise<void> => {
     await seedSuits(client);
     await seedValues(client);
+    await seedCards(client);
+};
+
+const seedCards = async (client: PrismaClient): Promise<void> => {
+    const suits = await client.suit.findMany();
+    const values = await client.value.findMany();
+
+    for (const suit of suits) {
+        for (const value of values) {
+            await client.card.create({ data: { 
+                suit: { connect: { name: suit.name } },
+                value: { connect: { name: value.name } },
+            }}); 
+        }
+    };
+
+    console.log('--- Seeded Deck ---')
 };
 
 const seedSuits = async (client: PrismaClient): Promise<void> => {
